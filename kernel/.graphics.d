0759473dd9f1bb7b7107801e409d605ee8d04414
Shota Sato <sugergg@gmail.com>
2021-08-06 07:25:58 +0900

    osbook_day06c: Mouse input with polling.

--- kernel/.graphics.d
+++
@@ -1,3 +0,0 @@
-graphics.o: graphics.cpp graphics.hpp frame_buffer_config.hpp \
-  /home/shota/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
-  /home/shota/osbook/devenv/x86_64-elf/include/c++/v1/__config
